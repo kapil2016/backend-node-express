4eeda1d99d107c1226abcff4b22ea89fe7ccf664
kapil kumar <kapilkumardu.ramjas@gmail.com>
2023-06-03 07:11:06 +0530

    route to diffrent pages

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const http = require('http')
 
 const server = http.createServer((req , res)=>{
-    console.log('kapil kumar')
+    // console.log('kapil kumar')
+    res.setHeader('Content-Type','text/html')
+    if(req.url === '/home'){
+        res.write('<html><head></head><body><h1>Welcome Home</h1></body></html>')
+    }else if(req.url === '/about'){
+        res.write('<html><head></head><body><h1>Welcome to about us Page</h1></body></html>')
+
+    }else if(req.url ==='/node'){
+        res.write('<html><head></head><body><h1>This is my node project</h1></body></html>')
+    }else{
+        res.write('<html><head></head><body><h1>Hello world</h1></body></html>')
+    }
+    res.end()
 })
 
 server.listen(4000)

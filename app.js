3acbd2b0086e2acf6b25546e0776d9cb7fbfa570
kapil kumar <kapilkumardu.ramjas@gmail.com>
2023-06-03 12:25:04 +0530

    reading and writing to the file

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require('http')
+const fs = require('fs')
+
+
 
 const server = http.createServer((req , res)=>{
-    // console.log('kapil kumar')
+    
     res.setHeader('Content-Type','text/html')
     if(req.url === '/home'){
         res.write('<html><head></head><body><h1>Welcome Home</h1></body></html>')
@@ -10,10 +13,42 @@ const server = http.createServer((req , res)=>{
 
     }else if(req.url ==='/node'){
         res.write('<html><head></head><body><h1>This is my node project</h1></body></html>')
-    }else{
-        res.write('<html><head></head><body><h1>Hello world</h1></body></html>')
+    }else if(req.url === '/message' && req.method === 'POST'){
+        const chunkedData = [];
+        req.on('data' , (chunk)=>{
+            chunkedData.push(chunk);
+        })
+        req.on('end',()=>{
+            const bufferdOutput = Buffer.concat(chunkedData).toString();
+            const message = bufferdOutput.split('=')[1];
+            fs.writeFileSync('message.txt' , message);
+
+        })
+        res.statusCode = 302 ;
+        res.setHeader('Location' , '/')
+        return res.end();
     }
-    res.end()
+    else{
+        fs.readFile('./message.txt', 'utf8', (err, data) => {
+            if (err) {
+              res.statusCode = 500;
+              res.end('Error reading the file.');
+              return;
+            }
+            res.statusCode = 200;
+            res.write("<html><head></head><body>")
+            res.write("<p>")
+            res.write(data)
+            res.write("</p>")
+            res.write("<form action='/message' method='POST'><input name='message'></input><button type='submit'> send </button></form>")
+            res.write('</body></html>')
+            return res.end();
+          })
+       
+    }
+
+    // res.end();
+    
 })
 
 server.listen(4000)

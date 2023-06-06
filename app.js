86a7258a2f293533b8b4baca2a11c5fdaea1020e
kapil kumar <kapilkumardu.ramjas@gmail.com>
2023-06-06 16:48:59 +0530

    work with Middlewares

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const http = require('http');
 const express = require('express')
 const app = express();
+const bodyParser = require('body-parser')
 
-app.use((req , res , next)=>{
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use("/add-product",(req , res , next)=>{
     console.log('in the middileware')
-    next();
+    res.send('<form action="/product" method="POST"><input type="text" name="product"></input><input type="text" name="size"></input><button type="submit">submit</button></form>')
+    
 })
-app.use((req , res , next)=>{
-    console.log('in the secound middileware')
-    res.send("<h1>Hello world</h1>")
+app.use("/product",(req , res , next)=>{
+    console.log(req.body)
+    res.redirect('/add-product')
+})
+app.use("/",(req , res , next)=>{
+  res.send('<h1>hello there !</h1>')
+
 })
 
 // const server = http.createServer(app)
